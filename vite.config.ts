194806418f78caffecbdb17156fd0bfa6867fa83
dfakhrizaman <daffafakhrizaman@gmail.com>
2026-06-24 23:33:36 +0700

    fix base route

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // GitHub Pages project site: https://<org>.github.io/notive-landing/
-  base: '/notive-landing/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
